2faedadee72e9cfc0f48b1c25e17da158a4d74ba
cryptolake <dhiadah@gmail.com>
2023-03-27 13:20:54 +0100

    straight.el

--- init.el
+++ init.el
@@ -1,11 +1,12 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
 (setq ring-bell-function 'ignore)
+(cua-mode t)
 (defun crypt/display-startup-time ()
   (message "Emacs loaded in %s with %d garbage collections."
 	   (format "%.2f seconds"
 		   (float-time
-		     (time-subtract after-init-time before-init-time)))
+		    (time-subtract after-init-time before-init-time)))
 	   gcs-done))
 
 (add-hook 'emacs-startup-hook #'crypt/display-startup-time)
@@ -24,22 +25,29 @@
 (global-auto-revert-mode t)
 
 ;; Initialize package sources
-(require 'package)
+(setq straight-repository-branch "develop")
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+;; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;; Install use-package
+(straight-use-package 'use-package)
 
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
+;; Configure use-package to use straight.el by default
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
 
 ;; backup and auto-save files in one single room
 
@@ -62,9 +70,9 @@
 
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
-		 term-mode-hook
-		 shell-mode-hook
-		 eshell-mode-hook))
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (set-face-attribute 'default nil :font "Hack" :height 120)
@@ -72,88 +80,88 @@
 (defun meow-setup ()
   (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
   (meow-motion-overwrite-define-key
-    '("j" . meow-next)
-    '("k" . meow-prev)
-    '("<escape>" . ignore))
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
   (meow-leader-define-key
-    ;; SPC j/k will run the original command in MOTION state.
-    '("j" . "H-j")
-    '("k" . "H-k")
-    ;; Use SPC (0-9) for digit arguments.
-    '("1" . meow-digit-argument)
-    '("2" . meow-digit-argument)
-    '("3" . meow-digit-argument)
-    '("4" . meow-digit-argument)
-    '("5" . meow-digit-argument)
-    '("6" . meow-digit-argument)
-    '("7" . meow-digit-argument)
-    '("8" . meow-digit-argument)
-    '("9" . meow-digit-argument)
-    '("0" . meow-digit-argument)
-    '("/" . meow-keypad-describe-key)
-    '("?" . meow-cheatsheet))
+   ;; SPC j/k will run the original command in MOTION state.
+   '("j" . "H-j")
+   '("k" . "H-k")
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
   (meow-normal-define-key
-    '("0" . meow-expand-0)
-    '("9" . meow-expand-9)
-    '("8" . meow-expand-8)
-    '("7" . meow-expand-7)
-    '("6" . meow-expand-6)
-    '("5" . meow-expand-5)
-    '("4" . meow-expand-4)
-    '("3" . meow-expand-3)
-    '("2" . meow-expand-2)
-    '("1" . meow-expand-1)
-    '("-" . negative-argument)
-    '(";" . meow-reverse)
-    '("," . meow-inner-of-thing)
-    '("." . meow-bounds-of-thing)
-    '("[" . meow-beginning-of-thing)
-    '("]" . meow-end-of-thing)
-    '("a" . meow-append)
-    '("A" . meow-open-below)
-    '("b" . meow-back-word)
-    '("B" . meow-back-symbol)
-    '("c" . meow-change)
-    '("d" . meow-delete)
-    '("D" . meow-backward-delete)
-    '("e" . meow-next-word)
-    '("E" . meow-next-symbol)
-    '("f" . meow-find)
-    '("g" . meow-cancel-selection)
-    '("G" . meow-grab)
-    '("h" . meow-left)
-    '("H" . meow-left-expand)
-    '("i" . meow-insert)
-    '("I" . meow-open-above)
-    '("j" . meow-next)
-    '("J" . meow-next-expand)
-    '("k" . meow-prev)
-    '("K" . meow-prev-expand)
-    '("l" . meow-right)
-    '("L" . meow-right-expand)
-    '("m" . meow-join)
-    '("n" . meow-search)
-    '("o" . meow-block)
-    '("O" . meow-to-block)
-    '("p" . meow-yank)
-    '("q" . meow-quit)
-    '("Q" . meow-goto-line)
-    '("r" . meow-replace)
-    '("R" . meow-swap-grab)
-    '("s" . meow-kill)
-    '("t" . meow-till)
-    '("u" . meow-undo)
-    '("U" . meow-undo-in-selection)
-    '("v" . meow-visit)
-    '("w" . meow-mark-word)
-    '("W" . meow-mark-symbol)
-    '("x" . meow-line)
-    '("X" . meow-goto-line)
-    '("y" . meow-save)
-    '("Y" . meow-sync-grab)
-    '("z" . meow-pop-selection)
-    '("'" . repeat)
-    '("<escape>" . ignore)))
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)))
 
 (use-package meow)
 (meow-setup)
@@ -163,13 +171,13 @@
   :init (global-undo-tree-mode))
 
 (use-package company
-	     :init (global-company-mode)
-	     (setq company-global-modes '(not org-mode))
-	     :bind (:map company-active-map
-			 ("<tab>" . company-complete-selection))
-	     :custom
-	     (company-minimum-prefix-length 1)
-	     (company-idle-delay 0.0))
+  :init (global-company-mode)
+  (setq company-global-modes '(not org-mode))
+  :bind (:map company-active-map
+	      ("<tab>" . company-complete-selection))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
 
 (defun text-mode-hook-setup ()
   ;; make `company-backends' local is critcal
@@ -185,11 +193,11 @@
 
 ;; Add git  to the side
 (use-package git-gutter-fringe
-	     :init
-	     (global-git-gutter-mode t))
+  :init
+  (global-git-gutter-mode t))
 
 (use-package rainbow-delimiters
-	     :hook (prog-mode . rainbow-delimiters-mode))
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package tree-sitter-langs)
 
@@ -206,44 +214,44 @@
 	eshell-scroll-to-bottom-on-input t))
 
 (use-package eshell
-	     :hook (eshell-first-time-mode . crypt/configure-eshell)
-	     :config
+  :hook (eshell-first-time-mode . crypt/configure-eshell)
+  :config
 
-	     (with-eval-after-load 'esh-opt
-				   (setq eshell-destroy-buffer-when-process-dies t)))
+  (with-eval-after-load 'esh-opt
+    (setq eshell-destroy-buffer-when-process-dies t)))
 
 (use-package which-key
-	     :init (which-key-mode)
-	     :diminish which-key-mode
-	     :config
-	     (setq which-key-idle-delay 0.3))
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
 
 (use-package vertico
-	     :init (vertico-mode)
-	     :demand t
-	     :bind (
-		    :map vertico-map
-		    ("C-j" . vertico-next)
-		    ("C-k" . vertico-previous))
-	     :config
-	     (setq vertico-sycle t))
+  :init (vertico-mode)
+  :demand t
+  :bind (
+	 :map vertico-map
+	 ("C-j" . vertico-next)
+	 ("C-k" . vertico-previous))
+  :config
+  (setq vertico-sycle t))
 
 (use-package savehist
-	     :init
-	     (savehist-mode))
+  :init
+  (savehist-mode))
 
 (use-package orderless
-	     :init
-	     (setq completion-styles '(orderless)
-		   completion-category-defaults nil
-		   completion-category-overrides '((file (styles partial-completion)))))
+  :init
+  (setq completion-styles '(orderless)
+	completion-category-defaults nil
+	completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package marginalia
-	     :bind (
-		    :map minibuffer-local-map
-		    ("M-A" . marginalia-cycle))
-	     :init
-	     (marginalia-mode))
+  :bind (
+	 :map minibuffer-local-map
+	 ("M-A" . marginalia-cycle))
+  :init
+  (marginalia-mode))
 
 (defun crypt/org-mode-setup ()
   (org-indent-mode 1)
@@ -251,41 +259,41 @@
   (visual-line-mode 1))
 
 (use-package org
-	     :hook (org-mode . crypt/org-mode-setup)
-	     :config
-	     (setq org-agenda-files
-		   '("~/Documents/org/todo.org"))
-	     (setq org-agenda-start-with-log-mode t)
-	     (setq org-log-done 'time)
-	     (setq org-ellipsis " ▾")
+  :hook (org-mode . crypt/org-mode-setup)
+  :config
+  (setq org-agenda-files
+	'("~/Documents/org/todo.org"))
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-ellipsis " ▾")
 
-	     (setq org-capture-templates
-		   `(("t" "Tasks / Projects")
-		     ("tt" "Task" entry (file+olp "~/Documents/org/todo.org" "Inbox")
-		      "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
+  (setq org-capture-templates
+	`(("t" "Tasks / Projects")
+	  ("tt" "Task" entry (file+olp "~/Documents/org/todo.org" "Inbox")
+	   "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
 
-		     ("j" "Journal Entries")
-		     ("jj" "Journal" entry
-		      (file+olp+datetree "~/Documents/org/journal.org")
-		      "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
-		      :clock-in :clock-resume
-		      :empty-lines 1)
-		     ("jm" "Meeting" entry
-		      (file+olp+datetree "~/Documents/org/journal.org")
-		      "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
-		      :clock-in :clock-resume
-		      :empty-lines 1))))
+	  ("j" "Journal Entries")
+	  ("jj" "Journal" entry
+	   (file+olp+datetree "~/Documents/org/journal.org")
+	   "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
+	   :clock-in :clock-resume
+	   :empty-lines 1)
+	  ("jm" "Meeting" entry
+	   (file+olp+datetree "~/Documents/org/journal.org")
+	   "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
+	   :clock-in :clock-resume
+	   :empty-lines 1))))
 
 (use-package org-bullets
-	     :after org
-	     :hook (org-mode . org-bullets-mode)
-	     :custom
-	     (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 (org-babel-do-load-languages
-  'org-babel-load-languages
-  '((emacs-lisp . t)
-    (python . t)))
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (python . t)))
 
 (require 'org-tempo)
 (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
@@ -298,16 +306,16 @@
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
-	     :hook (org-mode . crypt/org-mode-visual-fill))
+  :hook (org-mode . crypt/org-mode-visual-fill))
 
 (use-package dired
-	     :ensure nil
-	     :commands (dired dired-jump)
-	     :custom ((dired-listing-switches "-agho --group-directories-first")))
+  :straight (:type built-in)
+  :commands (dired dired-jump)
+  :custom ((dired-listing-switches "-agho --group-directories-first")))
 
 (use-package dired-single)
 (use-package all-the-icons
-	     :if (display-graphic-p))
+  :if (display-graphic-p))
 (use-package all-the-icons-dired
   :hook (dired-mode . all-the-icons-dired-mode))
 
@@ -347,8 +355,8 @@
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp))))  ; or lsp-deferred
+                         (require 'lsp-pyright)
+                         (lsp))))  ; or lsp-deferred
 
 (setq lsp-pyright-use-library-code-for-types nil) ;; set this to nil if getting too many false positive type errors
 (setq lsp-pyright-stub-path (concat (getenv "HOME") "/src/python-type-stubs")) ;; example
@@ -371,7 +379,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-gruvbox t)
+  (load-theme 'doom-gruvbox-light t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
@@ -389,7 +397,6 @@
 ;; Tree sitter support
 (global-tree-sitter-mode)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
-
 
 ;; smart parens
 (use-package smartparens
