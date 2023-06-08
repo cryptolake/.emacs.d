fc4353f1feeec8b60d05df1f4f309933853ccf77
cryptolake <dhiadah@gmail.com>
2023-06-08 22:28:46 +0100

    FINALLY FOUND THE ANSWER

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 		    (time-subtract after-init-time before-init-time)))
 	   gcs-done))
 
+
 (add-hook 'emacs-startup-hook #'crypt/display-startup-time)
 
 ;; Silence native compiler warnings
@@ -77,98 +78,40 @@
 
 (set-face-attribute 'default nil :font "Hack" :height 120)
 
-(defun meow-setup ()
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-  (meow-motion-overwrite-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
-  (meow-leader-define-key
-   ;; SPC j/k will run the original command in MOTION state.
-   '("j" . "H-j")
-   '("k" . "H-k")
-   ;; Use SPC (0-9) for digit arguments.
-   '("1" . meow-digit-argument)
-   '("2" . meow-digit-argument)
-   '("3" . meow-digit-argument)
-   '("4" . meow-digit-argument)
-   '("5" . meow-digit-argument)
-   '("6" . meow-digit-argument)
-   '("7" . meow-digit-argument)
-   '("8" . meow-digit-argument)
-   '("9" . meow-digit-argument)
-   '("0" . meow-digit-argument)
-   '("/" . meow-keypad-describe-key)
-   '("?" . meow-cheatsheet))
-  (meow-normal-define-key
-   '("0" . meow-expand-0)
-   '("9" . meow-expand-9)
-   '("8" . meow-expand-8)
-   '("7" . meow-expand-7)
-   '("6" . meow-expand-6)
-   '("5" . meow-expand-5)
-   '("4" . meow-expand-4)
-   '("3" . meow-expand-3)
-   '("2" . meow-expand-2)
-   '("1" . meow-expand-1)
-   '("-" . negative-argument)
-   '(";" . meow-reverse)
-   '("," . meow-inner-of-thing)
-   '("." . meow-bounds-of-thing)
-   '("[" . meow-beginning-of-thing)
-   '("]" . meow-end-of-thing)
-   '("a" . meow-append)
-   '("A" . meow-open-below)
-   '("b" . meow-back-word)
-   '("B" . meow-back-symbol)
-   '("c" . meow-change)
-   '("d" . meow-delete)
-   '("D" . meow-backward-delete)
-   '("e" . meow-next-word)
-   '("E" . meow-next-symbol)
-   '("f" . meow-find)
-   '("g" . meow-cancel-selection)
-   '("G" . meow-grab)
-   '("h" . meow-left)
-   '("H" . meow-left-expand)
-   '("i" . meow-insert)
-   '("I" . meow-open-above)
-   '("j" . meow-next)
-   '("J" . meow-next-expand)
-   '("k" . meow-prev)
-   '("K" . meow-prev-expand)
-   '("l" . meow-right)
-   '("L" . meow-right-expand)
-   '("m" . meow-join)
-   '("n" . meow-search)
-   '("o" . meow-block)
-   '("O" . meow-to-block)
-   '("p" . meow-yank)
-   '("q" . meow-quit)
-   '("Q" . meow-goto-line)
-   '("r" . meow-replace)
-   '("R" . meow-swap-grab)
-   '("s" . meow-kill)
-   '("t" . meow-till)
-   '("u" . meow-undo)
-   '("U" . meow-undo-in-selection)
-   '("v" . meow-visit)
-   '("w" . meow-mark-word)
-   '("W" . meow-mark-symbol)
-   '("x" . meow-line)
-   '("X" . meow-goto-line)
-   '("y" . meow-save)
-   '("Y" . meow-sync-grab)
-   '("z" . meow-pop-selection)
-   '("'" . repeat)
-   '("<escape>" . ignore)))
-
-(use-package meow)
-(meow-setup)
-(meow-global-mode 1)
-
 (use-package undo-tree
   :init (global-undo-tree-mode))
+
+
+(setq evil-want-keybinding 'nil)
+
+(use-package evil
+  :init (evil-mode 1))
+
+(use-package evil-commentary
+  :init (evil-commentary-mode))
+
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode 1))
+
+
+(use-package evil-collection
+  :init (evil-collection-init))
+
+;; THE BEST ELISP EVER
+
+(defun enable-translation ()
+  "Enable key translation."
+  (define-key key-translation-map (kbd "SPC") 'event-apply-control-modifier))
+
+(defun disable-translation ()
+  "Disable key translation."
+  (define-key key-translation-map (kbd "SPC") nil))
+
+(add-hook 'evil-normal-state-entry-hook #'enable-translation)
+(add-hook 'evil-normal-state-exit-hook #'disable-translation)
+
+(evil-set-undo-system 'undo-tree)
 
 (use-package company
   :init (global-company-mode)
@@ -334,12 +277,11 @@
 (setq interprogram-cut-function 'wl-copy)
 (setq interprogram-paste-function 'wl-paste)
 (use-package flycheck
-  :ensure t
   :init (global-flycheck-mode))
 
 (use-package docker
-  :ensure t
   :defer t)
+
 
 (use-package lsp-mode
   :init
@@ -353,7 +295,6 @@
 (use-package lsp-ui)
 
 (use-package lsp-pyright
-  :ensure t
   :hook (python-mode . (lambda ()
                          (require 'lsp-pyright)
                          (lsp))))  ; or lsp-deferred
@@ -364,22 +305,29 @@
 (setq lsp-pyright-typechecking-mode "basic")
 
 (use-package pyvenv
-  :ensure t
+  
   :init
   (setenv "WORKON_HOME" "~/.pyenv/versions")
   (add-hook 'python-mode-hook #'pyvenv-mode))
+
+(use-package conda
+  :init
+  ;; if you want interactive shell support, include:
+  (conda-env-initialize-interactive-shells)
+  ;; if you want eshell support, include:
+  (conda-env-initialize-eshell))
 (use-package ein)
 (use-package treemacs
-  :ensure t
+  
   :defer t)
 
 (use-package doom-themes
-  :ensure t
+  
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-gruvbox-light t)
+  (load-theme 'doom-gruvbox t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
@@ -402,3 +350,6 @@
 (use-package smartparens
   :init (show-smartparens-global-mode)
   :config (require 'smartparens-config))
+
+(custom-set-variables
+ '(markdown-command "/usr/bin/pandoc"))
